56e49a96ef8bebd14353fcca74319db530b3c97f
Augusto Kestering Longo <augusto.kestering@gmail.com>
2020-02-03 22:51:06 -0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [tech, setTech] = useState(['ReactJS', 'React Native']);
+  const [tech, setTech] = useState([]);
   const [newValue, setNewTech] = useState('');
 
   function handleADD() {
     setTech([...tech, newValue]);
     setNewTech('');
   }
+
+  useEffect(() => {
+    const storageTech = JSON.parse(localStorage.getItem('tech'));
+
+    if (storageTech) {
+      setTech(storageTech);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('tech', JSON.stringify(tech));
+  }, [tech]);
+
   return (
     <>
       <ul>
